47e7c28cdec582c8f92c98e0b04d88651a69700d
Roscoe Pyell <roscoejp@gmail.com>
2017-08-07 08:45:39 -0700

    Update eternitytower.js

--- eternitytower.js
+++ eternitytower.js
@@ -12,17 +12,19 @@
     'use strict';
 // === SCRIPT
 // CONFIG
-var debug = false;				// used for debug purposes (TODO)
+var debug = false;			// used for debug purposes (TODO)
 var mainInterval = 10;			// Timer interval for main (in seconds)[10 seconds]
 var abilityInterval = 1; 		// Timer interval for abilities (in seconds)[1 second]
 var farmingFrequency = 6;		// Frequency to check farms (number of iterations of main)[1 min]
 var healthThreshold = 60;		// Health before moving on to adventures (percentage of max)[60% health]
-var fullHelathThreshold = 90;	// Health before moving from adventures to combat (percentage of max)[90% health]
+var fullHelathThreshold = 90;		// Health before moving from adventures to combat (percentage of max)[90% health]
 var eneregyThreshold = 1;		// Energy threshold before moving to adventures (absolute value)[1 energy]
+var reloadFrequency = 360		// Frequency to reload the page to fix memory errors (number of iterations of main)[1 hour]
 
 // HELPERS
 var abilityTimer;
 var farmingIntervalCount = 0;
+var reloadIntervalConut = 0;
 var capsLockDown = false;
 $(document).on( "keydown", function(event) {if (event.which == 20) { capsDown = true; }});
 $(document).on( "keyup", function(event) {if (event.which == 20) { capsDown = false; }});
@@ -87,21 +89,21 @@ function doCombat() {
 	var isQuestActive = (parseFloat($(".d-flex.flex-wrap.align-items-center > .cancel-adventure > div").text().trim())) < 100 ? true : false;
 
 	if (!isInCombat) {		// figure out what to do since we're not in battle
-			if (!isQuestActive) {
-				if (isEnergyBarLow || isHealthLow) {
-					// quest until we have energy/health
-					healthCheck = fullHelathThreshold;
-					doAdventure();
+		if (!isQuestActive) {
+			if (isEnergyBarLow || isHealthLow) {
+				// quest until we have energy/health
+				healthCheck = fullHelathThreshold;
+				doAdventure();
 
-				} else {
-					// actually battle and change health threshold to group battles and quests
-					$(".btn.btn-primary.mt-1.random-battle").click();
-					healthCheck = healthThreshold;
-					useAbilities();
-				}
+			} else {
+				// actually battle and change health threshold to group battles and quests
+				$(".btn.btn-primary.mt-1.random-battle").click();
+				healthCheck = healthThreshold;
+				useAbilities();
 			}
 		}
-		else if(isInCombat) { useAbilities(); }
+	}
+	else if(isInCombat) { useAbilities(); }
 }
 
 // FARMING
@@ -145,9 +147,15 @@ function doFarming(){
 
 // MAIN TIMER
 var mainTimer = setInterval(function() {
-	if (!capsLockDown) {							// do nothing if caps is pressed
+	if (!capsLockDown) {					// do nothing if caps is pressed
 		if (location.pathname == "/combat") { 		// do combat stuff
 
+			// reload occassionally to fix memory errors
+			if (++reloadIntervalCount >= reloadFrequency) {
+				reloadFrequency = 0;
+				location.reload();
+			}
+			
 			// check crops at regular intervals
 			if (++farmingIntervalCount >= farmingFrequency) {
 				farmingIntervalCount = 0;
